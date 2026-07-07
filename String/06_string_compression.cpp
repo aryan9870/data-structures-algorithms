1612b8cd33bc74e97bd6b1e16aa32447337b4cbf
Aryan <aryan7017n@gmail.com>
2026-07-07 15:35:06 +0530

    string compression

---
+++ String/06_string_compression.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<char> chars = {'a','a','b','b','c', 'd', 'd', 'd'};
+    int idx = 0;
+    
+    for(int i = 0; i < chars.size(); i++) {
+        char ch = chars[i];
+        int count = 0;
+        while (chars[i] == ch && i < chars.size()) {
+           count++;
+           i++;
+        }
+
+        if(count == 1) {
+            chars[idx] = ch;
+            idx++;
+        } else {
+            chars[idx] = ch;
+            idx++;
+            string str = to_string(count);
+            for(int i = 0; i < str.size(); i++) {
+                chars[idx] = str[i];
+                idx++;
+            }
+        }
+        i--;
+    }
+
+    for(int i = 0; i < idx; i++) {
+        cout << chars[i] << " ";
+    }
+}

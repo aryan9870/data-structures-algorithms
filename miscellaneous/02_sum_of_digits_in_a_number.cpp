91928e55c33ce05cc93c86e6ede38b9bfce2f811
Aryan <aryan7017n@gmail.com>
2026-07-09 17:01:11 +0530

    sum of digits in a number

---
+++ miscellaneous/02_sum_of_digits_in_a_number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 121;
+
+    int sum = 0;
+    while(n > 0) {
+        int lastDigit = n % 10;
+        sum += lastDigit;
+        n /= 10;
+    }
+
+    cout << sum;
+    
+}
